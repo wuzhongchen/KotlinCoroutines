01d6662dd56b56959b89ec0df53809ef3552a3b3
neymarr11 <871361324@qq.com>
2023-02-14 17:00:07 +0800

    测试离线包下载

--- kotlincoroutineflow/src/main/java/com/dongnaoedu/flowpractice/fragment/DownloadFragment.kt
+++ kotlincoroutineflow/src/main/java/com/dongnaoedu/flowpractice/fragment/DownloadFragment.kt
@@ -19,7 +19,12 @@ import java.io.File
 
 class DownloadFragment : Fragment() {
 
-    val URL = "http://192.168.1.4:8080/kotlinstudyserver/pic.JPG"
+    //离线包地址
+    val URL = "http://img1.suiyuexiaoshuo.com/mcdn/offline_pack/android/pack/offline_v1000029.zip?ver=1000029&t=1675913807";
+
+    val URL2 = "https://r1---sn-i3b7knzs.c.2mdn.net/videoplayback/id/6edccd1852eccfc1/itag/347/source/web_video_ads/ctier/L/acao/yes/ip/0.0.0.0/ipbits/0/expire/1707460925/sparams/acao,ctier,expire,id,ip,ipbits,ipbypass,itag,mh,mip,mm,mn,ms,mv,mvi,pl,source/signature/CA441504C9AE75CCA71574D9C55A994C43671D.3BFB5BCC9BE2EB85605107CD513CD4478BF2C90B/key/cms1/mh/gL/pl/36/redirect_counter/1/rm/sn-i3bdk7e/req_id/944d5f79289a3ee/cms_redirect/yes/ipbypass/yes/mip/2407:cdc0:aa4a:2cfe:5c62:ee73:6bbf:bfe2/mm/42/mn/sn-i3b7knzs/ms/onc/mt/1675924612/mv/m/mvi/1?file=file.mp4";
+
+    val URL3 = "https://r1---sn-i3b7knzs.c.2mdn.net/videoplayback/id/6edccd1852eccfc1/itag/347/source/web_video_ads/ctier/L/acao/yes/ip/0.0.0.0/ipbits/0/expire/1707460925/sparams/acao,ctier,expire,id,ip,ipbits,ipbypass,itag,mh,mip,mm,mn,ms,mv,mvi,pl,source/signature/CA441504C9AE75CCA71574D9C55A994C43671D.3BFB5BCC9BE2EB85605107CD513CD4478BF2C90B/key/cms1/mh/gL/pl/36/redirect_counter/1/rm/sn-i3bdk7e/req_id/944d5f79289a3ee/cms_redirect/yes/ipbypass/yes/mip/2560:2cfe:cdc0:aa4a:5c62:bfe2:ee73:6bbf/mm/42/mn/sn-i3b7knzs/ms/onc/mt/1675924612/mv/m/mvi/1?file=file.mp4";
 
     private val mBinding: FragmentDownloadBinding by lazy {
         FragmentDownloadBinding.inflate(layoutInflater)
@@ -32,12 +37,14 @@ class DownloadFragment : Fragment() {
         return mBinding.root
     }
 
+
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
 
         lifecycleScope.launchWhenCreated {
             context?.apply {
-                val file = File(getExternalFilesDir(null)?.path, "pic.JPG")
+                val file = File(getExternalFilesDir(null)?.path, "offline.zip")
+//                val file = File(getExternalFilesDir(null)?.path, "video.mp4")
                 DownloadManager.download(URL, file).collect { status ->
                     when (status) {
                         is DownloadStatus.Progress -> {
@@ -47,17 +54,17 @@ class DownloadFragment : Fragment() {
                             }
                         }
                         is DownloadStatus.Error -> {
-                            Toast.makeText(context, "下载错误", Toast.LENGTH_SHORT).show()
+                            Log.d("ningli", "下载错误$status")
                         }
                         is DownloadStatus.Done -> {
                             mBinding.apply {
                                 progressBar.progress = 100
                                 tvProgress.text = "100%"
                             }
-                            Toast.makeText(context, "下载完成", Toast.LENGTH_SHORT).show()
+                            Log.d("ningli", "下载完成")
                         }
                         else -> {
-                            Log.d("ning", "下载失败.")
+                            Log.d("ningli", "下载失败.")
                         }
                     }
                 }
